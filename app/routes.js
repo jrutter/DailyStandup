6bf1726e40ff6d1be2f6a3eebbb614786cb5cd28
Jake Rutter <jake@onerutter.com>
2018-02-14 07:03:05 -0500

    Added Feature route and started work on creating api to get data to front-end

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,33 @@
 // app/routes.js
+
+// import axios from 'axios'
+
+
 module.exports = function(app, passport) {
 
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+
+
     app.get('/', function(req, res) {
         res.render('index.ejs', {
           user : req.user // get the user out of session and pass to template
-        }); // load the index.ejs file
+        });
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
+    app.get('/features', function(req, res) {
+        res.render('features.ejs', {
+          user : req.user
+        });
+    });
+
+    app.get('/list', function(req, res) {
+        // res.render('list.ejs', {
+        //   user : req.user,
+        //   test: 'test'
+        // });
+        res.json('test')
+    });
+
+
     app.get('/login', function(req, res) {
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') });
@@ -26,12 +40,7 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
     app.get('/signup', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', {
           message: req.flash('signupMessage'),
@@ -46,20 +55,12 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
