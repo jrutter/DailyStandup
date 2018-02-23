ec59ca04769af109b7e490ef47f26ba099a7f84d
Jake Rutter <jake@onerutter.com>
2018-02-23 06:51:03 -0500

    Updated status model

--- app/models/status.js
+++ app/models/status.js
@@ -8,7 +8,24 @@ var statusSchema = mongoose.Schema({
         team: String,
         blocker: String,
         today: String,
-        yesterday: String
+        yesterday: String,
+        created_at: Date,
+        updated_at: Date
+});
+
+// on every save, add the date
+statusSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
 });
 
 // create the model for users and expose it to our app
